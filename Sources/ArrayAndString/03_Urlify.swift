4b6b4292299c54ec7b968224064319d15bd70176
Ikhsan Assaat <ixnixnixn@yahoo.com>
2017-12-28 08:31:14 +0000

    Implement urlify

--- Sources/ArrayAndString/03_Urlify.swift
+++ Sources/ArrayAndString/03_Urlify.swift
@@ -1,10 +1,33 @@
 import Foundation
 
 public func urlify(_ str: String, _ size: Int) -> String {
-    return ""
+    let word = str.trimmingCharacters(in: .whitespacesAndNewlines)
+
+    var stringSize = size
+    for char in word {
+        if char == " " { stringSize += 2 }
+    }
+
+    var chars: [Character] = Array(repeating: Character("_"), count: stringSize)
+    var index = 0
+
+    for char in word {
+        if char != " " {
+            chars[index] = char
+            index += 1
+        } else {
+            chars[index] = "%"
+            chars[index+1] = "2"
+            chars[index+2] = "0"
+            index += 3
+        }
+    }
+
+    return String(chars)
 }
 
 /*
  Learnings:
- - 
+ - Misunderstood the true size in the question
+ - Don't forget trimming whitespaces
 */
