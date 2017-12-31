e2006604cddc893ebac0507cb48608e25aecdb5b
Ikhsan Assaat <ixnixnixn@yahoo.com>
2017-12-31 07:55:05 +0000

    Solve string compression

--- Sources/ArrayString/06_StringCompression.swift
+++ Sources/ArrayString/06_StringCompression.swift
@@ -1,5 +1,40 @@
 import Foundation
 
+/// Complexity: O(n) time and space
 public func compress(_ str: String) -> String {
-    return ""
+    let strCount = str.utf8.count
+    var chars: [Character] = Array(repeating: "_", count: strCount)
+    var current: Character? = nil
+    var counter = 0
+    var index = 0
+
+    for char in str {
+        if char != current {
+            if counter > 0 {
+                chars[index] = Character("\(counter)")
+                index += 1
+                if index >= strCount { return str }
+            }
+
+            chars[index] = char
+            index += 1
+            if index >= strCount { return str }
+
+            current = char
+            counter = 0
+        }
+        counter += 1
+    }
+
+    if counter > 0 {
+        chars[index] = Character("\(counter)")
+    }
+
+    return String(chars.prefix(index + 1))
 }
+
+/*
+ Learnings:
+ - Making many String concats is not efficient
+ - In C/Java, terminating String is easier, in Swift we just cut the string until index
+ */
--- Tests/CrackingTests/ArrayStringTests.swift
+++ Tests/CrackingTests/ArrayStringTests.swift
@@ -79,8 +79,8 @@ class ArrayStringTests: XCTestCase {
 
      Implement a method to perform basic string compression using the counts of repeated characters. For example, the string `aabccccaaa` would become `a2b1c5a3`. If the 'compressed' string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a -z)
      */
-    func _test_06_StringCompression() {
-        XCTAssertEqual(compress("aabccccaaa"), "a2b1c5a3")
+    func test_06_StringCompression() {
+        XCTAssertEqual(compress("aabccccaaa"), "a2b1c4a3")
         XCTAssertEqual(compress("aa"), "a2")
         XCTAssertEqual(compress("abcde"), "abcde")
     }
