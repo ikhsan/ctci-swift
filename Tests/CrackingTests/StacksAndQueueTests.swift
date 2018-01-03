7b81357b82671d891f441bdeef5f4e2785a742bf
Ikhsan Assaat <ixnixnixn@yahoo.com>
2018-01-03 22:19:54 +0000

    Add stacks & queues problem

--- Tests/CrackingTests/StacksAndQueueTests.swift
+++ Tests/CrackingTests/StacksAndQueueTests.swift
@@ -6,6 +6,27 @@ import XCTest
 class StacksAndQueuesTests: XCTestCase {
 
     /**
+     1: Three in One
+
+     Describe how you could use a single array to implement three stacks
+     */
+    func _test_01_ThreeInOne() {}
+
+    /**
+     2: Stack Min
+
+     How would you design a stack which, in addition to `push` and `pop`, has a function `min` which returns the minimum element? `Push`, `pop` and `min` should all operate in O(1) time
+     */
+    func _test_02_StackMin() {}
+
+    /**
+     3: Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure `SetOfStacks` that mimics this. `SetOfStacks` should be composed of several stacks and should create a new stack once the previous one exceeds capacity. `SetOfStacks.push()` and `SetOfStacks.pop()` should behave identically to a single stack (that is, `pop()` should return the same values as it would if there were just a single stack).
+
+     **FOLLOW UP**: Implement a function `popAt(int index)` which performs a pop operation on a specific sub-stack.
+     */
+    func _test_03_StackOfPlates() {}
+
+    /**
      4: Queue via Stacks
 
      Implement a MyQueue class which implements a queue using two stacks
@@ -34,5 +55,18 @@ class StacksAndQueuesTests: XCTestCase {
         XCTAssert(subject.isEmpty)
     }
 
+    /**
+     5: Sort Stack
+
+     Write a program to sort a stack such that the smallest items are on the top. You can use an additional temporary stack, but you may not copy the elements into any other data structure (such as an array). The stack supports the following operations: `push`, `pop`, `peek`, and `isEmpty`.
+     */
+    func test_05_SortStack() {}
+
+    /**
+     6: Animal Shelter
+
+     An animal shelter, which holds only dogs and cats, operates on a stricly "first in, first out" basis. People must adopt either the "oldest" (based on arrival time) of all animals at the shelter, or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of that type). They cannot select which specific animal they would like. Create the data structures to maintain this system and impelement operations such as `enqueue`, `dequeueuAny`, `dequeueDog`, and `dequeueCat`. You may use the built-in LInkedList data structure.
+     */
+    func test_06_AnimalShelter() {}
 }
 
