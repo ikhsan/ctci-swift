4171afa9c57531353cdd2c34d4bb1ea5238e9faf
Ikhsan Assaat <ixnixnixn@yahoo.com>
2018-01-04 00:12:56 +0000

    Solve sum linked list

--- Sources/LinkedListsQuestions/05_SumLists.swift
+++ Sources/LinkedListsQuestions/05_SumLists.swift
@@ -1,1 +1,67 @@
-import Foundation
+import DataStructure
+
+/// Complexity: O(n) time & space
+public func sumList(_ lhs: LinkedList<Int>, _ rhs: LinkedList<Int>) -> LinkedList<Int> {
+    let result = LinkedList<Int>()
+
+    var leftCurrent = lhs.head
+    var rightCurrent = rhs.head
+    var node: LinkedList<Int>.Node? = nil
+
+    var carry = 0
+    while leftCurrent != nil || rightCurrent != nil {
+        let sum = (leftCurrent?.value ?? 0) + (rightCurrent?.value ?? 0) + carry
+
+        carry = sum / 10
+        let number = sum % 10
+
+        let newNode = LinkedList.Node(number)
+        if result.head == nil {
+            result.head = newNode
+        } else {
+            node?.next = newNode
+        }
+
+        node = newNode
+        leftCurrent = leftCurrent?.next
+        rightCurrent = rightCurrent?.next
+    }
+
+    if carry > 0 {
+        node?.next = .init(carry)
+    }
+
+    return result
+}
+
+/// Complexity: O(n) time & space
+public func sumListReverse(_ lhs: LinkedList<Int>, _ rhs: LinkedList<Int>) -> LinkedList<Int> {
+    return intToList(listToInt(lhs) + listToInt(rhs))
+}
+
+private func listToInt(_ list: LinkedList<Int>) -> Int {
+    return list.reduce(into: 0) { (result, node) in
+        result = (result * 10) + node.value
+    }
+}
+
+private func intToList(_ number: Int) -> LinkedList<Int> {
+    let list = LinkedList<Int>()
+    var num = number
+    while num > 0 {
+        let node = LinkedList<Int>.Node(num % 10)
+        node.next = list.head
+
+        list.head = node
+        num /= 10
+    }
+    return list
+}
+
+// TODO: Recursion
+
+/*
+ Learnings:
+ - Reduce into is awesome
+ - Didn't think of recursion as the solution
+ */
--- Tests/CrackingTests/LinkedListsTests.swift
+++ Tests/CrackingTests/LinkedListsTests.swift
@@ -72,7 +72,7 @@ class LinkedListsTests: XCTestCase {
     /**
      5: Sum Lists
 
-     You have two numbers represented by a linked list, where each node containes a single digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
+     You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
 
 
      Input: (7 -> 1 -> 6) + (5 -> 9 -> 2). That is, 617 + 295
@@ -82,7 +82,13 @@ class LinkedListsTests: XCTestCase {
      Input: (6 -> 1 -> 7) + (2 -> 9 -> 5). That is, 617 + 295
      Output: 9 -> 1 -> 2. That is, 912.
      */
-    func _test_05_SumLists() {}
+    func test_05_SumLists() {
+        XCTAssertEqual(sumList([7, 1, 6], [5, 9 ,2]), [2, 1, 9])
+        XCTAssertEqual(sumList([9, 9, 9], [1]), [0, 0, 0, 1])
+
+        XCTAssertEqual(sumListReverse([6, 1, 7], [2, 9, 5]), [9, 1, 2])
+        XCTAssertEqual(sumListReverse([9, 9, 9], [1]), [1, 0, 0, 0])
+    }
 
     /**
      6: Palindrome
