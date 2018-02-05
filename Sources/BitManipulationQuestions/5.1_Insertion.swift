482bffa47d559919a62a8e500a4eaebceabe0370
Ikhsan Assaat <ixnixnixn@yahoo.com>
2018-02-05 21:26:29 +0000

    Solve bit insertion

--- Sources/BitManipulationQuestions/5.1_Insertion.swift
+++ Sources/BitManipulationQuestions/5.1_Insertion.swift
@@ -4,23 +4,25 @@ public func insertBit(n: Int, m: Int, i: Int, j: Int) -> Int {
     let mask = createMask(from: i, to: j)
     let nCleared = n & mask
     let mShifted = m << i
-    return 0
+    return nCleared | mShifted
 }
 
 private func createMask(from start: Int, to end: Int) -> Int {
-    let left = createMask(to: start)
-    let right = createMask(from: start + end)
-
-    // print(String(allOnes))
-    print(String(right, radix: 2))
-    return left | right
+    return createMask(to: start) | createMask(from: start + end)
 }
 
-func createMask(to i: Int) -> Int {
+private func createMask(to i: Int) -> Int {
     return (1 << i) - 1
 }
 
-func createMask(from i: Int) -> Int {
-    let allOnes = 0b1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111
-    return allOnes << i
+private func createMask(from i: Int) -> Int {
+    return ~0 << (i - 1)
 }
+
+/**
+
+Learnings:
+- Get, set, clear bits
+- Print and assign integer in binary: `0b1111` and `String(s, radix: 2)`
+
+*/
